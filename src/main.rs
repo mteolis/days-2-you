b3c964446004d99a4f987e4b63322ec081a81302
Michael Teolis <michael.teolis@outlook.com>
2023-09-02 12:16:03 -0400

    Add fn to read email creds from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 extern crate chrono;
 
+use std::fs;
 use chrono::{Utc, DateTime, TimeZone};
 
 fn main() {
     println!("Hello, Days2You!");
     get_days_to();
+    let (username, password) = read_credentials();
+    println!("Username: {}", username);
+    println!("Password: {}", password);
+}
+
+fn read_credentials() -> (String, String) {
+    let filename = "credentials";
+    println!("Reading from {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let lines: Vec<&str> = contents.split("\n").collect();
+    let username: &str = lines[0].split("=").collect::<Vec<_>>()[1];
+    let password: &str = lines[1].split("=").collect::<Vec<_>>()[1];
+
+    return (username.trim().to_string(), password.trim().to_string());
 }
 
 fn get_days_to() {
