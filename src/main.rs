de4c9fdd6d1e3515542c9e6c5dae381856210f12
Michael Teolis <michael.teolis@outlook.com>
2023-09-17 21:48:55 -0400

    wip: create smtp mailer and transport

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate chrono;
+extern crate lettre;
 
 use std::fs;
 use chrono::{Utc, DateTime, TimeZone};
+use lettre::{Message, SmtpTransport, Transport, transport::smtp::authentication::Credentials};
+// use lettre::message::{header::ContentType, Message};
 
 fn main() {
     println!("Hello, Days2You!");
+    // build_message();
+    // send_message(build_message());
     get_days_to();
+    // read_credentials();
+    let message = build_message();
+
     let (username, password) = read_credentials();
-    println!("Username: {}", username);
-    println!("Password: {}", password);
+
+    let credentials = Credentials::new(username.to_string(), password.to_string());
+
+    // TODO: set up SMTP server
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(credentials)
+        .build();
+
+    match mailer.send(&message) {
+        Ok(_) => println!("Email sent successfully"),
+        Err(e) => println!("Could not send email: {:?}", e),
+    }
+    // println!("Username: {}", username);
+    // println!("Password: {}", password);
 }
 
 fn read_credentials() -> (String, String) {
@@ -37,3 +58,14 @@ fn get_days_to() {
 fn get_days_between(start_date: DateTime<Utc>, end_date: DateTime<Utc>) -> i64 {
     return (end_date - start_date).num_days();
 }
+
+fn build_message() -> Message {
+
+    println!("Message built:\n{}", std::str::from_utf8(&message.formatted()).unwrap());
+
+    return message;
+}
+
+// fn send_message(message: Message) {
+//     let sender = SendmailTransport::new();
+// }
